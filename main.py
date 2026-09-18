1040780f0087c15b848c00d1e8d2d25c18f471fc
rakshan29 <raksh2901@gmail.com>
2026-09-18 20:12:39 +0530

    Initial commit: First API Call

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from google import genai
+
+client=genai.Client()
+
+response = client.models.generate_content(
+	model="gemini-2.5-flash",
+	contents="Explain how AI works in a few words"
+)
+print(response.text)
+
