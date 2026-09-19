c04828de1cd36a72c4bec39345645e28138a5eac
rakshan29 <raksh2901@gmail.com>
2026-09-19 19:52:38 +0530

    Updated chat session to use send_message_stream

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ chat = client.chats.create(model="gemini-3.6-flash")
 
 # Ask anything anytime inside the chat.send_message("....")
 
-response = chat.send_message("Explain Onepiece")
+response = chat.send_message_stream("Explain Onepiece")
 
-print(response.text)
+for chunk in response:
+	print(chunk.text,end="",flush=True)
+print()
