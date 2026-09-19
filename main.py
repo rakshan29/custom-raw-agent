96f6f92978a019bc309a9a67890ddf20d76db782
rakshan29 <raksh2901@gmail.com>
2026-09-19 18:54:09 +0530

    Upgraded agent to use stateful client.chats interface

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ client=genai.Client()
 
 chat = client.chats.create(model="gemini-3.6-flash")
 
-response = chat.send_message("Explain how AI works in a few words"
-)
+response = chat.send_message("Explain how AI works in a few words")
 
 print(response.text)
 
