7c02f732e5efdd1b844b11ad279dcca75c92dc56
rakshan29 <raksh2901@gmail.com>
2026-09-19 20:05:05 +0530

    Cleaned the code

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import os
 from google import genai
 from dotenv import load_dotenv
 
+# Loads secrets variables
 load_dotenv()
 
+# Initiate Gemini Client
 client=genai.Client()
 
 chat = client.chats.create(model="gemini-3.6-flash")
 
-# Ask anything anytime inside the chat.send_message("....")
-
+# Ask anything anytime inside chat.send_message("....")
 response = chat.send_message_stream("Explain Onepiece")
 
+# Iterate through text chunks as they arrive from Google's servers
+# Human reading sychronization, Real-Time Feedback & Cancellation
 for chunk in response:
 	print(chunk.text,end="",flush=True)
 print()
