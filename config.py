06b1617fa4d785e98310fa032e041910230f72c2
rakshan29 <raksh2901@gmail.com>
2026-09-22 11:17:10 +0530

    fix: an AttributeError fixed

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Retrieves KEY safely from .env file
-api.key=os.getenv("GEMINI_API_KEY")
+api_key=os.getenv("GEMINI_API_KEY")
 
 if not api_key:
 	raise ValueError("GEMINI_API_KEY is missing! Please set it in your .env file.")
